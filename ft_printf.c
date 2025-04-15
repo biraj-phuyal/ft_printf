aab961ba1abeb3753342e0803844f622fe306cab
biraj-phuyal <biphuyal@c1r12s5.42lisboa.com>
2025-04-15 14:06:36 +0100

    Adding base

---
+++ ft_printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	if (!format)
+		return (-1);
+	return (write(1, format, ft_strlen(format)));
+}
+
+int	main(void)
+{
+	int returned = ft_printf( "0", 923489792, -239483, "fsefwef", NULL);
+	printf("FT_PRINTF RETURNED %d\n", returned);
+}
